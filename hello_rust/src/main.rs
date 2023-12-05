8f5062e9131110804f52b7910e4f5dc120485d62
Ed <edenilson.filho1998@gamil.com>
2023-12-04 21:41:37 -0400

    work return json

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,5 +1,32 @@
 use std::io::stdin;
-fn main() {
+use serde_json;
+use serde::{Deserialize,Serialize};
+
+#[derive(Debug,Serialize,Deserialize)]
+// #[serde(rename_all = "camelCase")]
+struct JsonPara {
+    // #[serde(rename = "camelCase")]
+    // abilities: Vec<String>,
+    base_experience: i32,
+    // forms: Vec<String>,
+    // game_indices: Vec<String>,
+    height: i32,
+    // held_items: Vec<String>,
+    id: i32,
+    is_default: bool,
+    location_area_encounters: String,
+    // moves: Vec<String>,
+    name: String,
+    order: i32,
+    // past_abilities: Vec<String>,
+    // past_types: Vec<String>,
+    // species: Vec<String>,
+    // sprites: Vec<String>,
+    // stats: Vec<String>,
+    // types: Vec<String>,
+    weight:i32,
+}
+fn main() -> Result<(),reqwest::Error>{
     let mut poke_number = String::new();
     println!("Enter a number between 1 and 1292");
     println!("Poke Number: ");
@@ -13,18 +40,10 @@ fn main() {
         Err(_err) => 0i32
     };
     
-    if poke_number > 1292{
-        println!("Nao funcionou, Nao tem pokemons com esse numero");
-    }else {
-        let url = format!("https://pokeapi.co/api/v2/pokemon/{}",poke_number);
-        let body = match reqwest::blocking::get(url){
-          Ok(r) => r,
-          Err(_err) => {
-            println!("Request failed: {}", _err.to_string());
-            return;
-          }  
-        };
-        println!("{:?}",body.text())
-    }
+    let url = format!("https://pokeapi.co/api/v2/pokemon/{}",poke_number);
+    let body:JsonPara = reqwest::blocking::get(url).unwrap().json().unwrap();    
+    // let body:JsonPara  = serde::de::Deserialize::deserialize(body).unwrap();
+    println!("{:#?}",body);
+    Ok(())
 
 }
